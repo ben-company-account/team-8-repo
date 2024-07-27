5574fdb595740c620470cd4181ea7b8f37a2f12d
Ben Asperheim <ben13786@esri.com>
2024-07-27 09:13:08 -0700

    pushing to my branch test

--- src/runtime/widget.tsx
+++ src/runtime/widget.tsx
@@ -2,32 +2,52 @@ import { AllWidgetProps } from "jimu-core"
 import React from "react"
 import { JimuMapView, JimuMapViewComponent } from "jimu-arcgis"
 import { useState } from "react"
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer"
+import Feature from "@arcgis/core/widgets/Feature"
 
 type State = {
-    jimuMapView: JimuMapView
+    
 }
 
 const Widget = (props: AllWidgetProps<unknown>): React.ReactElement => {
 
-    const [state, setState] = useState<State>({ jimuMapView: null })
+    const [state, setState] = useState<State>()
+
+    const [mapView, setMapView] = useState<JimuMapView>(null); 
 
     const determineOrdering = (): void => {
+        
+    }
 
+    const getFeatures = async () => {
+        if (mapView){
+            await mapView.view.when()
+            const allEditableLayers: __esri.Collection<FeatureLayer | __esri.SceneLayer | __esri.SubtypeGroupLayer> = mapView.view.map.editableLayers
+
+            // Will hopefully just be one feature layer 
+            var featureLayers: __esri.Collection<FeatureLayer> 
+            allEditableLayers.map((lay) => {
+                if (lay instanceof FeatureLayer){
+                    featureLayers.add(lay)
+                }
+            })
+
+            const featureLayer: FeatureLayer = featureLayers.at(0)
+            
+
+
+        }
     }
 
 
 
     const activeViewChangeHandler = (jmv: JimuMapView): void => {
         if (jmv) {
-            setState((prevState) => ({
-                ...prevState, jimuMapView: jmv
-            }))
+            setMapView(jmv)
         }
 
         else {
-            setState((prevState) => ({
-                ...prevState, jimuMapView: null
-            }))
+            setMapView(jmv)
         }
     }
 
