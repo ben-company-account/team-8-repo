dbc491fdf2a82ade20e456909b37b384f29963ad
Ben Asperheim <ben13786@esri.com>
2024-07-27 09:56:27 -0700

    hopefully working

--- src/runtime/widget.tsx
+++ src/runtime/widget.tsx
@@ -4,39 +4,51 @@ import { JimuMapView, JimuMapViewComponent } from "jimu-arcgis"
 import { useState } from "react"
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer"
 import Feature from "@arcgis/core/widgets/Feature"
+import Graphic from "@arcgis/core/Graphic"
 
 type State = {
-    
+
 }
 
 const Widget = (props: AllWidgetProps<unknown>): React.ReactElement => {
 
     const [state, setState] = useState<State>()
 
-    const [mapView, setMapView] = useState<JimuMapView>(null); 
+    const [mapView, setMapView] = useState<JimuMapView>(null);
+
+    const [allFeatures, setAllFeatures] = useState<Graphic[]>(null)
 
     const determineOrdering = (): void => {
-        
+
     }
 
-    const getFeatures = async () => {
-        if (mapView){
+    const updateAllFeatures = async (): Promise<void> => {
+
+
+        if (mapView) {
             await mapView.view.when()
             const allEditableLayers: __esri.Collection<FeatureLayer | __esri.SceneLayer | __esri.SubtypeGroupLayer> = mapView.view.map.editableLayers
 
             // Will hopefully just be one feature layer 
-            var featureLayers: __esri.Collection<FeatureLayer> 
+            var featureLayers: __esri.Collection<FeatureLayer>
             allEditableLayers.map((lay) => {
-                if (lay instanceof FeatureLayer){
+                if (lay instanceof FeatureLayer) {
                     featureLayers.add(lay)
                 }
             })
 
             const featureLayer: FeatureLayer = featureLayers.at(0)
-            
+            featureLayer.queryFeatures().then((res: __esri.FeatureSet) => {
 
+                setAllFeatures(res.features)
+
+            })
 
         }
+        else {
+            setAllFeatures(null)
+        }
+
     }
 
 
