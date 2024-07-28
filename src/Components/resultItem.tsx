964fb44db49e220f9081edbc73011004074301ea
Ben Asperheim <ben13786@esri.com>
2024-07-27 22:55:15 -0700

    mabe

--- src/Components/resultItem.tsx
+++ src/Components/resultItem.tsx
@@ -1,7 +1,7 @@
 import Graphic from "@arcgis/core/Graphic"
 import { JimuMapView } from "jimu-arcgis"
 import React from "react"
-import { CalciteListItem } from "calcite-components"
+import { CalciteListItem, CalciteAction } from "calcite-components"
 
 const ResultItem = ({graphic, mapView}): React.ReactElement => {
 
@@ -10,9 +10,20 @@ const ResultItem = ({graphic, mapView}): React.ReactElement => {
         mapView.view.extent = graphic.geometry.extent
     }
 
+    const questionMarkClickHandler = async (): Promise<void> => {
+        
+    }
+
     return (
         // change this to CalciteListItem and make on click OnCalciteListItemSelect={()=> zoomTo
         <CalciteListItem onCalciteListItemSelect={zoomTo} label={graphic.attributes.parkname}>
+            <CalciteAction slot="actions-end"
+            text=""
+            icon="question-mark"
+            scale="s"
+            onClick={questionMarkClickHandler}
+            />
+
             
         </CalciteListItem>
     )
