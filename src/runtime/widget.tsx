6c140d2b2144cb10936f31f76039b0c40eced849
Ben Asperheim <ben13786@esri.com>
2024-07-27 18:51:10 -0700

    stupid directory?

--- src/runtime/widget.tsx
+++ src/runtime/widget.tsx
@@ -16,7 +16,9 @@ import { useState } from "react"
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer"
 import Feature from "@arcgis/core/widgets/Feature"
 import Graphic from "@arcgis/core/Graphic"
-import { CalciteButton } from "calcite-components"
+import { CalciteButton } from "calcite-components"  
+import { useEffect } from "react"
+import ResultItem from "../Components/resultItem"
 
 type State = {
 
@@ -32,63 +34,90 @@ const Widget = (props: AllWidgetProps<unknown>): React.ReactElement => {
 
     const [importantAttributes, setImportantAttributes] = useState<string[]>()
 
+    const [topTenFeatures, setTopTenFeatures] = useState<Graphic[]>(null)
+
     const run = (): void => {
         determineOrdering()
+        console.log(importantAttributes)
     }
 
-  const determineOrdering = (): void => {
-    console.log("SDFDS")
-    // For each feature / graphic in our feature layer
-    if (allFeatures) {
-        console.log("SDFSDF")
-      allFeatures.map((graph) => {
-        var score = 0;
+    const determineOrdering = (): void => {
+        console.log("SDFDS")
+        // For each feature / graphic in our feature layer
+        if (allFeatures) {
+            console.log("SDFSDF")
+            allFeatures.map((graph) => {
+                var score = 0;
 
                 // Go through every attribute in the feature 
-                graph.attributes.map((prop: string) => {
 
-                    // If the feature attribute ends in _score
-                    if (prop.endsWith("_score")) {
+                const graphAttributes: JSON = graph.attributes
+                console.log(graphAttributes)
 
-                        // If that attribute is actually important to the teacher 
-                        if (importantAttributes.includes(prop)) {
-                            score += graph.attributes[prop] * (importantAttributes.length - importantAttributes.indexOf(prop))
+                Object.keys(graphAttributes).forEach((key: string) => {
+                    if (key.endsWith("_score")){
+                        if (importantAttributes.includes(key)) {
+                            score += graph.attributes[key] * (importantAttributes.length - importantAttributes.indexOf(key))
                         }
-
                     }
-                })
+                  });
 
                 // Sets the score of the graphic 
                 graph.attributes.score = score
             })
         }
+
+        console.log(allFeatures)
+
+        allFeatures.sort((a, b) => b.attributes.score - a.attributes.score)
+        const topTen: Graphic[] = allFeatures.slice(0, 10)
+
+        setTopTenFeatures(topTen)
+
     }
 
-  const updateAllFeatures = async (jmv: JimuMapView): Promise<void> => {
-    if (jmv && jmv.view) {
-      console.log(jmv)
-      console.log("HERE");
-      await jmv.view.when();
-      const allEditableLayers: __esri.Collection<
-        FeatureLayer | __esri.SceneLayer | __esri.SubtypeGroupLayer
-      > = jmv.view.map.editableLayers;
+    const updateAllFeatures = async (jmv: JimuMapView): Promise<void> => {
+        if (jmv && jmv.view) {
+            console.log(jmv)
+            console.log("HERE");
+            await jmv.view.when();
+            const allEditableLayers: __esri.Collection<
+                FeatureLayer | __esri.SceneLayer | __esri.SubtypeGroupLayer
+            > = jmv.view.map.editableLayers;
 
-      console.log(allEditableLayers)
+            console.log(allEditableLayers.length)
 
-      // Will hopefully just be one feature layer
-      let featureLayers: __esri.Collection<FeatureLayer>
+            const layer = new FeatureLayer({
+                // URL to the service
+                url: "https://services8.arcgis.com/LLNIdHmmdjO2qQ5q/arcgis/rest/services/LA_County_Parks___Open_Spaces_WFL1/FeatureServer/4"
+            });
 
-            // Add layers of type FeatureLayer to the Colections object featureLayers 
-            for (const lay of allEditableLayers) {
-                if (lay.type === "feature") {
-                    featureLayers.add(lay)
-                }
-            }
+            console.log("SDFDSF")
+            await layer.load()
 
-            const featureLayer: FeatureLayer = featureLayers.at(0)
-            featureLayer.queryFeatures().then((res: __esri.FeatureSet) => {
+            // console.log(layer)
+            // console.log("SDFSDF")
+            // console.log(layer.type)
+
+
+            // // Will hopefully just be one feature layer
+            // let featureLayers: __esri.Collection<FeatureLayer>
+
+            // // Add layers of type FeatureLayer to the Colections object featureLayers 
+            // allEditableLayers.map((lay) => {
+            //     console.log("ESEHHSF")
+            //     if (lay.type === "feature") {
+            //         featureLayers.add(lay)
+            //     }
+            // })
+
+
+
+            layer.queryFeatures().then((res: __esri.FeatureSet) => {
+                console.log("HERE?")
 
                 setAllFeatures(res.features)
+
 
             })
 
@@ -100,14 +129,14 @@ const Widget = (props: AllWidgetProps<unknown>): React.ReactElement => {
     }
 
 
-
-
-  const activeViewChangeHandler = (jmv: JimuMapView): void => {
-    if (jmv) {
-      setMapView(jmv)
-      updateAllFeatures(jmv)
-    } else {
-      setMapView(jmv);
+    const activeViewChangeHandler = (jmv: JimuMapView): void => {
+        if (jmv) {
+            setMapView(jmv)
+            updateAllFeatures(jmv)
+            setImportantAttributes(["deer_score", "fern_pine_score"])
+        } else {
+            setMapView(jmv);
+        }
     }
 
     return (
@@ -121,6 +150,13 @@ const Widget = (props: AllWidgetProps<unknown>): React.ReactElement => {
                 useMapWidgetId={props.useMapWidgetIds?.[0]}
                 onActiveViewChange={activeViewChangeHandler}
             />
+
+            {topTenFeatures && 
+                topTenFeatures.map((graph) => (
+                    <ResultItem graphic={graph} mapView={mapView}/>
+                        
+                ))
+            }
 
 
         </div>
